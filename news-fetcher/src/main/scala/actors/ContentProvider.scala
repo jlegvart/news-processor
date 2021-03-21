37ba4659df9aff673bbb07b3acac862ba2e3f30d
jlegvart <jernej.legvart@protonmail.com>
2021-03-21 11:55:30 +0100

    postfix

--- news-fetcher/src/main/scala/actors/ContentProvider.scala
+++ news-fetcher/src/main/scala/actors/ContentProvider.scala
@@ -13,6 +13,7 @@ import service.RSSContentProviderService
 
 import scala.collection.mutable
 import scala.concurrent.duration._
+import scala.language.postfixOps
 import scala.util.{Failure, Success}
 
 
